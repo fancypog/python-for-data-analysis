f7de7a8e3e22b40053aff48e3d20050c365badda
Frances <135648147+fancypog@users.noreply.github.com>
2024-04-01 19:03:50 +0100

    Update notes.py

--- notes.py
+++ notes.py
@@ -11,5 +11,9 @@ sales.info()
 // Understand the statistical properties of the data
 sales.describe()  // for quick statistical overview
 
+// Numerical analysis and visualisation
+sales['Unit_Cost'].describe() //for a spacific column
+sales['Unit_Cost'].mean()
+sales['Unit_Cost'].median()
 
 
