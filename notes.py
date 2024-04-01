597038e009636a6554e22c2f579b03d2682d891f
Frances <135648147+fancypog@users.noreply.github.com>
2024-04-01 18:37:39 +0100

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,1 +1,15 @@
+// Import data
+import pandas as pd
+sales = pd.read_csv('sales_data.csv', parse_dates = ['Date'])
+
+
+// Understand the structure of the data
+sales.head()
+sales.shape
+sales.info()
+
+// Understand the statistical properties of the data
+sales.describe()  // for quick statistical overview
+
+
 
