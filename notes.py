7a160bfbfd36e93e9ffeda7364f3429b23230007
Frances <135648147+fancypog@users.noreply.github.com>
2024-04-03 15:20:01 +0100

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,17 +1,17 @@
-// Import data
+# Import data
 import pandas as pd
 sales = pd.read_csv('sales_data.csv', parse_dates = ['Date'])
 
 
-// Understand the structure of the data
+# Understand the structure of the data
 sales.head()
 sales.shape
 sales.info()
 
-// Understand the statistical properties of the data
+# Understand the statistical properties of the data
 sales.describe()  // for quick statistical overview
 
-// Numerical analysis and visualisation
+# Numerical analysis and visualisation
 sales['Unit_Cost'].describe() //for a spacific column
 sales['Unit_Cost'].mean()
 sales['Unit_Cost'].median()
